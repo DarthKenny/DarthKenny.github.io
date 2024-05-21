ddca4ea25f21715498e9ded82a47ad7b8ae20c98
DarthKenny <164003258+DarthKenny@users.noreply.github.com>
2024-05-21 09:00:16 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,28 @@
-$(document).ready(function(){
+document.addEventListener("DOMContentLoaded", function() {
+    const gallery = document.querySelector(".gallery");
+    const prevButton = document.querySelector(".gallery-btn-prev");
+    const nextButton = document.querySelector(".gallery-btn-next");
+
+    let currentIndex = 0;
+    const maxIndex = gallery.children.length - 1;
+
+    prevButton.addEventListener("click", function() {
+      currentIndex = (currentIndex === 0) ? maxIndex : currentIndex - 1;
+      moveGallery();
+    });
+
+    nextButton.addEventListener("click", function() {
+      currentIndex = (currentIndex === maxIndex) ? 0 : currentIndex + 1;
+      moveGallery();
+    });
+
+    function moveGallery() {
+      const newPosition = currentIndex * -100 + "%";
+      gallery.style.transform = `translateX(${newPosition})`;
+    }
+  });
+  
+ $(document).ready(function(){
     $('#contact-form').on('submit', function(event){
         event.preventDefault(); // Остановить отправку формы обычным способом
 
