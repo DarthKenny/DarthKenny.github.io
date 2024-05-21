a7beb554f23fd5b70cb4fc42cd6779c7f2fa3811
DarthKenny <164003258+DarthKenny@users.noreply.github.com>
2024-05-21 09:00:55 +0300

    Update send_mail.php

--- send_mail.php
+++ send_mail.php
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->Host = 'smtp.yandex.ru';
         $mail->SMTPAuth = true;
         $mail->Username = 'infoprivatehouse@yandex.ru';
-        $mail->Password = 'jihcsurhspdvtfql';
+        $mail->Password = 'jihcsurhspdvtfql'; 
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
         
@@ -60,6 +60,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
-
 
 
